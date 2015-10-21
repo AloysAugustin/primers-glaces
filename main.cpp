4f567f731830dc459df8730785b98b530109100d
Aloys Augustin <aloys.augustin@polytechnique.org>
2015-10-21 19:41:12 +0200

    Simulated annealing test
    Works well

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ class Representation {
             positions[i][0] = rand_range(0,X-1);
             positions[i][1] = rand_range(0,Y-1);
         }
-        _value = -1.;
+        value();
     }
 
     double value() {
@@ -120,17 +120,13 @@ bool repr_sorter(Representation& lhs,Representation& rhs) {
     return lhs.value() < rhs.value();
 }
 
-void print_values(const Representation &r) {
-    cout << '{';
+void print_values(Representation &r, double time, double temp) {
+    cout << "Value " << r.value() << " time " << time << " temp " << temp << endl;
     for (int i = 0; i < NUM_GLACIERS; i ++) {
-        cout << '{' <<  r.positions[i][0] << ',' << r.positions[i][1] << "},";
+        cout <<  r.positions[i][1] << ',' << r.positions[i][0] << endl;
     }
 }
 
-#define GEN_SIZE 512
-#define NUM_GEN 5000
-#define MUT1_PROBA 0.01
-#define MUT2_PROBA 0.1
 
 #define MIN(A,B) (A <= B ? A : B)
 
@@ -138,47 +134,35 @@ int main()
 {
     srand(time(0));
     init_data();
-    std::vector<Representation> generation;
-    for (int i = 0; i < GEN_SIZE; i ++) {
-        generation.push_back(Representation());
-    }
+    Representation r = Representation();
+    Representation test = Representation();
+    double delta = 0.2;
+    double time = 0.;
+    double last_mod = 0.;
+    double temp = 0.;
+    double global_best = 1000.;
 
-    sort(generation.begin(),generation.end(),repr_sorter);
-    cout << "Best at initial generation: value " << generation[0].value() << endl;
-
-    for (int g = 0; g < NUM_GEN; g ++) {
-        //selection
-        int num_updated = GEN_SIZE / 4;
-        for (int i = GEN_SIZE / 4; i < (3*GEN_SIZE) / 4; i ++) {
-            if (rand01() > 2 * ((3*GEN_SIZE)/4 - i) / GEN_SIZE) {
-                generation[num_updated].copy(generation[i]);
-                num_updated ++;
+    while ((time - last_mod) <= 10000) {
+        if (r.value() < global_best) {
+            global_best = r.value();
+            cout << "New best found!" << endl;
+            print_values(r, time, temp);
+        }
+        test.copy(r);
+        test.mutate_soft();
+        double mod = test.value() - r.value();
+        if (mod < 0) {
+            r.copy(test);
+            last_mod = time;
+        } else {
+            temp = delta / log(time);
+            double probachg = exp(-mod / temp);
+            if (rand01() > probachg) {
+                r.copy(test);
+                last_mod = time;
             }
         }
-        int kept = num_updated-1;
-        // crossover
-        while (num_updated < GEN_SIZE) {
-            int r1 = rand_range(0,kept);
-            int r2 = rand_range(0,kept);
-            generation[num_updated].fromParents(&generation[r1],&generation[r2]);
-            num_updated ++;
-        }
-        // mutation
-        for (int i = 0; i < GEN_SIZE; i ++) {
-            if (rand01() < MUT1_PROBA) {
-                generation[i].mutate_hard();
-            }
-            if (rand01() < MUT2_PROBA) {
-                generation[i].mutate_soft();
-            }
-        }
-        // evaluation
-        sort(generation.begin(),generation.end(),repr_sorter);
-        cout << "Best at generation " << g << " : value " << generation[0].value() << endl;
-    }
-    cout << "Final best: " << endl;
-    for (int i = 0; i < NUM_GLACIERS; i ++) {
-        cout << generation[0].positions[i][1] << "," << generation[0].positions[i][0] << endl;
+        time += 1.;
     }
 
     return 0;
